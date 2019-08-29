96f1fc7c1b8b293be29b164be2b78eb30315d781
Alain Tran <alaint77@gmail.com>
2019-08-29 22:37:14 +0200

    still learning

---
+++ part2/coursecontents/src/components/Course.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map(part => (
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.exercises,
+    0
+  );
+
+  return <h4>Total of {total} exercises</h4>;
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return <div></div>;
+};
+
+export default App;
