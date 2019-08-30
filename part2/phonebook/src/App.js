8bfd46015de717c08f578289857ec53ce2fa5448
Alain Tran <alaint77@gmail.com>
2019-08-30 18:53:38 +0200

    still cant FILTER phonebook's name....

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,65 @@
 import React, { useState } from "react";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
+  ]);
+  const [newName, setNewName] = useState("add new name..");
+  const [newNumber, setNewNumber] = useState("add number...");
 
-  return <div></div>;
+  const addName = event => {
+    event.preventDefault();
+
+    const checkExistingName = persons.filter(person => {
+      return person.name.includes(newName);
+    });
+
+    if (checkExistingName.length === 1) {
+      alert(`${newName} is already in your phonebook`);
+      setNewName("");
+      setNewNumber("");
+    } else {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      };
+
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleNameChange = event => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter />
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
+  );
 };
 
 export default App;
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Filter = () => {
+  return (
+    <p>
+      Filter shown with
+      <input value="trying..." />
+    </p>
+  );
+};
+
+export default Filter;
